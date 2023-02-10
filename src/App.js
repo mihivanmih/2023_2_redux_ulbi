6872c784866cd96dede469b6357fc151e58bba01
Gusb <mihivanmih@gmail.com>
2023-02-10 15:52:55 +0300

    Action, arr

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
 import './App.css'
 import { useDispatch, useSelector } from 'react-redux'
 import { useState } from 'react'
-import { cashReducer } from './store/cashReducer'
+import { addCustomerAction, removeCustomerAction } from './store/customerReduser'
 
 function App() {
     
     const dispatch = useDispatch()
     const cash = useSelector(state => state.cashReducer.cash)
+    const customers = useSelector(state => state.customerReducer.customer)
     
     const [cashInput, setCashInput] = useState(0)
     
     const addCash = () => {
         dispatch({type: "ADD_CASH", payload: Number(cashInput)})
     }
+    
     const getCash = () => {
         dispatch({type: "GET_CASH", payload: Number(cashInput)})
+    }
+    
+    const addCustomer = () => {
+        const params = {
+            name: cashInput,
+            id: Date.now()
+        }
+        dispatch(addCustomerAction(params))
+    }
+    
+    const removeCustomer = (id) => {
+        dispatch(removeCustomerAction(id))
     }
     
     return (
@@ -27,7 +41,26 @@ function App() {
                 placeholder={ 'введите сумму' }
                 onChange={ (e) => setCashInput(e.target.value) }
                 value={ cashInput }
-            />
+            /> <br/>
+            <button onClick={ addCustomer }>Добавить пользователя</button>
+    
+            {
+                customers.length > 0
+                    ?
+                    <div>
+                        {
+                            customers.map(customer => <div
+                                key={customer.id}
+                                onClick={() => removeCustomer(customer.id)}
+                            >{customer.name}</div>)
+                        }
+                    </div>
+                    :
+                    <div>
+                        Пользователи не найдены
+                    </div>
+            }
+            
         </div>
     )
 }
